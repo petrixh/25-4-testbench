6f569f58901841deeb7fa12010b99cf30b791239
Petri Heinonen <petri@vaadin.com>
2024-09-18 10:13:12 +0300

    Added additional simple test

--- src/test/java/com/example/application/SimpleIT.java
+++ src/test/java/com/example/application/SimpleIT.java
@@ -1,10 +1,13 @@
 package com.example.application;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import com.vaadin.flow.component.button.testbench.ButtonElement;
+import com.vaadin.flow.component.notification.testbench.NotificationElement;
+import com.vaadin.flow.component.textfield.testbench.TextFieldElement;
 import com.vaadin.testbench.BrowserTest;
 import com.vaadin.testbench.BrowserTestBase;
 
@@ -28,6 +31,29 @@ public class SimpleIT extends BrowserTestBase {
 
         // Check that text of the button is "Clicked"
         Assertions.assertEquals("Say hello", button.getText());
+    }
+
+    @BrowserTest
+    public void fillInAndClickButton() {
+
+        TextFieldElement textField = $(TextFieldElement.class).first();
+        textField.setValue("John Doe");
+
+        // Find the first button (<vaadin-button>) on the page
+        ButtonElement button = $(ButtonElement.class).first();
+
+        // Click it
+        button.click();
+
+        // Check that text of the button is "Clicked"
+        Assertions.assertEquals("Say hello", button.getText());
+
+        waitUntil(ExpectedConditions.presenceOfElementLocated(By.tagName("vaadin-notification")));
+        // Assert that a notification with the correct message is shown
+        NotificationElement notificationElement = $(NotificationElement.class).first(); 
+        //Assertions.assertTrue(notificationElement.isDisplayed());
+        Assertions.assertEquals("Hello John Doe", notificationElement.getText());
+
     }
 
 }
